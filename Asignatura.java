0807d4ff5c1fad5d73beda34ad1d7e65ca67df1e
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-10-03 21:40:35 -0600

    Update Asignatura.java

--- Asignatura.java
+++ Asignatura.java
@@ -1,16 +1,39 @@
 public class Asignatura {
+
+    // Atributos o miembros
     private String nombreAsig;
     private String carreraAsig;
     private String clave;
     static int contAsig = 0;
 
+    // Constructor
     public Asignatura(String nombreAsig, String carreraAsig, String clave){
         this.nombreAsig = nombreAsig;
         this.carreraAsig = carreraAsig;
         this.clave = clave;
         contAsig ++;
     }
-    public int Contador(){
+    //Métodos del Objeto
+    public String getNameAsig(){
+        return nombreAsig;
+    }
+    public String getCarreraAsig(){
+        return carreraAsig;
+    }
+    public String getClave(){
+        return clave;
+    }
+    public void setCarreraAsig(String carreraAsig){
+        this.carreraAsig = carreraAsig;
+    }
+    public void setClave(String clave){
+        this.clave = clave;
+    }
+
+    //Metodo de la clase
+    public static int getCont(){
         return contAsig;
     }
+
+
 }
