88118957546bfb17ff33ec49987d44d7efa1efb1
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-10-04 19:53:52 -0600

    Impresion

--- ClasesdelPrograma/Grupo.java
+++ ClasesdelPrograma/Grupo.java
@@ -41,9 +41,13 @@ public class Grupo {
 
     public void Imprimir(){
         System.out.println("---------- Datos del Grupo "+ getNo_Grupo() +" ----------\n");
-        System.out.println("Materia: "+ getAsignatura().getNameAsig());
-        System.out.println("Carrera: "+ getAsignatura().getCarreraAsig());
-        System.out.println("Profesor: "+ getProfesor().getNameP());
-        System.out.println("Horario: "+ getProfesor().getHorario());
+        materia.ImprimirDatos();
+        profe.ImprimirDatos();
+        int i = 1;
+        for(Alumno alu : alumnos){
+            System.out.println("\n---------- Alumno "+ i +" ----------");
+            alu.Imprimir();
+            i ++;
+        }
     }
 }
